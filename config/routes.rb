3cae447e1b989734779d184dcf99892a3dd7df92
Deji Hastrup <deji.hastrup@gmail.com>
2021-08-23 16:09:32 +0100

    testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create, :show, :edit, :update]
+
+  resources :islands, only: [:index, :show, :new, :create, :edit, :update]
+  resources :bookings, only: [:index, :show, :edit]
+
+
+  resources :islands, only: [:index] do
+      resources :bookings, only: [:new, :create]
+  end
 end
