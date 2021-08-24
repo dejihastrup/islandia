abc4f1e7b658c702ac0c2be531c858b15226963a
Rebecca Cheung <rebeccalycc@gmail.com>
2021-08-24 18:36:57 +0100

    bookings link

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,29 @@
 class BookingsController < ApplicationController
 
-  def show
+  def index
+    @bookings = Booking.all
+    # where current user = user
   end
 
-  def index
+  def show
+    @booking = Booking.find(params[:id])
+    # where current user = user
   end
 
   def new
+    @booking = Booking.new
+    @island = Island.find(params[:island_id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.island = Island.find(params[:island_id])
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def update
@@ -17,4 +31,14 @@ class BookingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :confirmed, :user_id, :island_id, :created_at, :updated_at);
+  end
+   # t.datetime "created_at", precision: 6, null: false ???
+   # t.datetime "updated_at", precision: 6, null: false ???
+   # t.index ["island_id"], name: "index_bookings_on_island_id" ???
+   # t.index ["user_id"], name: "index_bookings_on_user_id" ???
 end
