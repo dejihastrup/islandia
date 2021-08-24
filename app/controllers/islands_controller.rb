7c9eccb7dffa9d234f4abb5deb3937dc468e1f61
Deji Hastrup <deji.hastrup@gmail.com>
2021-08-24 11:17:20 +0100

    finished routes and controller skeleton

---
+++ app/controllers/islands_controller.rb
@@ -0,0 +1,35 @@
+class IslandsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit();
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :islands, only: [:index, :show, :new, :create, :edit, :update]
-  resources :bookings, only: [:index, :show, :edit]
-
-
-  resources :islands, only: [:index] do
-      resources :bookings, only: [:new, :create]
+  resources :islands do
+    resources :bookings, only: [:new, :create]
   end
+
+  get '/my_islands', to: 'islands#index'
+
+  resources :bookings, only: [:index, :show]
 end
