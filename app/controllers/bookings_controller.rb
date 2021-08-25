28864a4c8fdae72fceb237f6e5a916bfc4673f29
Deji Hastrup <deji.hastrup@gmail.com>
2021-08-25 12:51:30 +0100

    booking index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -20,7 +20,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.confirmed = false
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to bookings_path
     else
       render :new
     end
